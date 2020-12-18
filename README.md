f1829f23a6a8c6b61c54bbe8414311f77571bd9c
Noah Snelson <noah.snelson@protonmail.com>
2020-12-17 23:18:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2020
+
+my attempt at solving the advent of code 2020 using clojure, a pretty neat Lisp
+built on top of the JVM.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject advent-2020 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2020 solutions"
+  :url "http://github.com/peeley/advent-2020"
+  :license {:name "GPL-3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns app.core})
