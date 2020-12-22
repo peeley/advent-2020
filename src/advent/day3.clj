351f2e210363176b1f4286a45c283b4774b10a34
Noah Snelson <noah.snelson@protonmail.com>
2020-12-21 20:59:06 -0800

    Solve part 2 of day 3

--- src/advent/day3.clj
+++ src/advent/day3.clj
@@ -7,7 +7,7 @@
     (nth (nth lines y) (mod x width))))
 
 (defn part1
-  [lines]
+  [lines x-slope y-slope]
   (let [height (count lines)]
     (loop [x 0
            y 0
@@ -15,6 +15,13 @@
       (if (>= y height)
         count
         (recur
-         (+ x 3)
-         (+ y 1)
+         (+ x x-slope)
+         (+ y y-slope)
          (if (= (get-tile lines x y) \#) (inc count) count))))))
+
+
+(defn part2
+  [input]
+  (reduce *
+          (map #(advent.day3/part1 input (first %) (second %))
+               [[1 1] [3 1] [5 1] [7 1] [1 2]])))
