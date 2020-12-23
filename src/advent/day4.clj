5d859543bbff1ee9c134b05de3d142ebe1a21b0b
Noah Snelson <noah.snelson@protonmail.com>
2020-12-22 21:42:54 -0800

    Solve part 1 of day4

--- src/advent/day4.clj
+++ src/advent/day4.clj
@@ -1,19 +1,19 @@
 (ns advent.day4
   (:require [clojure.string :as str]))
 
-(def required-fields '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"))
+(def required-fields #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"})
 
 (defn passport-fields-valid?
   [passport-fields]
-  (= passport-fields required-fields))
+  (= (conj passport-fields "cid") required-fields))
 
 (defn get-fields-from-passport
   [passport]
-  (map #(first (str/split % #":")) (str/split passport #"[ \n]")))
+  (into #{} (map #(first (str/split % #":")) (str/split passport #"[ \n]"))))
 
 (defn part1
   [input]
   (count
-   (filter passport-fields-valid?
-           (map get-fields-from-passport input))))
+    (filter passport-fields-valid?
+      (map get-fields-from-passport input))))
 
