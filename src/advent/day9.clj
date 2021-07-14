47714ed56581ed21b6d5520dc1f704ac24cbb920
Noah Snelson <noah.snelson@protonmail.com>
2021-07-13 19:54:01 -0700

    Solve part 2 of day 9

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -12,15 +12,36 @@
 
 (defn part1
   [input]
-  (for [idx (range 25 (count input))
-        :let [complement-set (subvec input (- idx 25) idx)
-              target (nth input idx)]
-        :when (not (subvec-contains-sum? complement-set target))]
-    target))
+  (first 
+    (for [idx (range 25 (count input))
+            :let [complement-set (subvec input (- idx 25) idx)
+                target (nth input idx)]
+            :when (not (subvec-contains-sum? complement-set target))]
+        target)))
 
 (def input (->> (slurp "test/advent/day9/input")
              (str/split-lines)
              (map #(bigint %))
              (vec)))
 
-(part1 input) 
+(def invalid-number (part1 input))
+
+(defn get-interval-indices
+  [input]
+  (loop [window-min 0
+         window-max 1]
+    (let [sum (apply + (subvec input window-min window-max))]
+      (cond
+        (= sum invalid-number) [window-min window-max]
+        (< sum invalid-number) (recur window-min (inc window-max))
+        (> sum invalid-number) (recur (inc window-min) window-max)))))
+
+(defn part2
+  [input]
+  (let [[start end] (get-interval-indices input)
+        subset (subvec input start end)
+        min (apply min subset)
+        max (apply max subset)]
+    (+ min max)))
+
+(part2 input)
