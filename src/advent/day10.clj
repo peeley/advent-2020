84bb18667049c4883b6c236ad341d4f99f2492b1
Noah Snelson <noah.snelson@protonmail.com>
2021-07-17 20:36:14 -0700

    Solve part 2 of day 10

--- src/advent/day10.clj
+++ src/advent/day10.clj
@@ -4,12 +4,13 @@
 (def input (->> (slurp "test/advent/day10/input")
                 (str/split-lines)
                 (map #(Integer/parseInt %))
+                (sort)
                 (vec)
-                (sort)))
+                (#(conj % (+ (reduce max %) 3))))) ;; last is always three higher than max
 
 (defn get-joltage-differences
   [input]
-  (loop [differences {1 0 2 0 3 1}
+  (loop [differences {1 0 2 0 3 0}
          last-joltage 0
          joltage-ratings input]
     (if (empty? joltage-ratings)
@@ -28,3 +29,21 @@
     (* one-jolt-diffs three-jolt-diffs)))
 
 (part1 input)
+
+(def count-combinations
+  (memoize
+    (fn [last-joltage current-idx]
+      (if (< current-idx (dec (count input)))
+        (let [current-joltage (get input current-idx)
+              next-joltage (get input (inc current-idx))]
+          (if (<= (- next-joltage last-joltage) 3)
+            (+ (count-combinations current-joltage (inc current-idx))
+               (count-combinations last-joltage (inc current-idx)))
+            (count-combinations current-joltage (inc current-idx))))
+        1))))
+
+(defn part2
+  []
+  (count-combinations 0 0))
+
+(part2)
