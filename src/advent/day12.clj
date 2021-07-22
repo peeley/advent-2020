a2d1be8df00a0c5daeba8d04d30aae5cf05e884c
Noah Snelson <noah.snelson@protonmail.com>
2021-07-22 13:22:53 -0700

    Solve part 1 of day 12

---
+++ src/advent/day12.clj
@@ -0,0 +1,33 @@
+(ns advent.day12
+  (:require [clojure.string :as str]))
+
+(defn parse-action
+  [action-str]
+  (let [direction (first action-str)
+        magnitude (Integer/parseInt (subs action-str 1))]
+    [direction magnitude]))
+
+(defn calc-coordinates
+  [x y angle magnitude]
+  [(+ x (* magnitude (Math/cos angle)))
+   (+ y (* magnitude (Math/sin angle)))
+   angle])
+
+(defn perform-action
+  [[x y angle] [direction magnitude]]
+  (case direction
+    \N [x (+ y magnitude) angle]
+    \S [x (- y magnitude) angle]
+    \E [(+ x magnitude) y angle]
+    \W [(- x magnitude) y angle]
+    \L [x y (+ angle (Math/toRadians magnitude))]
+    \R [x y (- angle (Math/toRadians magnitude))]
+    \F (calc-coordinates x y angle magnitude)))
+
+(def input (->> (slurp "test/advent/day12/input")
+                (str/split-lines)
+                (map parse-action)))
+(defn part1
+  []
+  (let [[x y _](reduce perform-action [0 0 0] input)]
+    (+ (Math/abs x) (Math/abs y))))
