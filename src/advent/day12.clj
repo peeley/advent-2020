259b7ef36c4db23113aee7e20125bdb1fa85972a
Noah Snelson <noah.snelson@protonmail.com>
2021-07-24 14:02:53 -0700

    Solve part 2 of day 12

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -31,3 +31,40 @@
   []
   (let [[x y _] (reduce perform-action [0 0 0] input)]
     (+ (Math/abs x) (Math/abs y))))
+
+(defn rotate-waypoint
+  [[waypoint-x waypoint-y] [direction magnitude]]
+  (let [angle (Math/atan2 waypoint-y waypoint-x)
+        hyp (Math/hypot waypoint-x waypoint-y)
+        new-angle (if (= direction \L)
+                    (+ angle (Math/toRadians magnitude))
+                    (- angle (Math/toRadians magnitude)))
+        new-x-dist (* hyp (Math/cos new-angle))
+        new-y-dist (* hyp (Math/sin new-angle))]
+    [new-x-dist new-y-dist]))
+
+(defn move-towards-waypoint
+  [[boat-x boat-y] [waypoint-x waypoint-y] magnitude]
+  (let [moved-x (* magnitude waypoint-x)
+        moved-y (* magnitude waypoint-y)]
+    [(+ boat-x moved-x)
+     (+ boat-y moved-y)]))
+
+(defn perform-waypoint-action
+  [[boat-coords waypoint-coords] action]
+  (let [[waypoint-x waypoint-y] waypoint-coords
+        [direction magnitude] action]
+   (case direction
+     \N [boat-coords [waypoint-x (+ waypoint-y magnitude)]]
+     \S [boat-coords [waypoint-x (- waypoint-y magnitude)]]
+     \E [boat-coords [(+ waypoint-x magnitude) waypoint-y]]
+     \W [boat-coords [(- waypoint-x magnitude) waypoint-y]]
+     \L [boat-coords (rotate-waypoint waypoint-coords action)]
+     \R [boat-coords (rotate-waypoint waypoint-coords action)]
+     \F [(move-towards-waypoint boat-coords waypoint-coords magnitude)
+         waypoint-coords])))
+
+(defn part2
+  [input]
+  (let [[[x y] _] (reduce perform-waypoint-action [[0 0] [10 1]] input)]
+    (+ (Math/abs x) (Math/abs y))))
