3c903ff5f7be5e4e4198321bc6bd870577e6cbd3
Noah Snelson <noah.snelson@protonmail.com>
2021-08-05 23:10:09 -0700

    Solve part 2 of day 11

--- src/advent/day11.clj
+++ src/advent/day11.clj
@@ -36,24 +36,27 @@
   (>= (count-adjacent-occupied seat-location seats) 4))
 
 (defn update-seat
-  [seat-location seats]
+  [seat-location seats crowded-pred empty-pred]
   (let [seat-state (get-seat-at seat-location seats)]
     (cond
       (and (= empty-seat seat-state)
-           (no-adjacent-occupied? seat-location seats)) occupied-seat
+           (empty-pred seat-location seats)) occupied-seat
       (and (= occupied-seat seat-state)
-           (crowded-seat? seat-location seats)) empty-seat
+           (crowded-pred seat-location seats)) empty-seat
       :else seat-state)))
 
 (defn step-state
-  [seats-state]
+  [seats-state crowded-pred empty-pred]
   (let [height (count seats-state)
         width (count (first seats-state))]
     (mapv vec
           (partition width
                      (for [y (range 0 height)
                            x (range 0 width)]
-                       (update-seat [y x] seats-state))))))
+                       (update-seat [y x]
+                                    seats-state
+                                    crowded-pred
+                                    empty-pred))))))
 
 (defn count-total-occupied
   [seats]
@@ -65,7 +68,72 @@
 (defn part1
   [input]
   (loop [before-state input]
-    (let [after-state (step-state before-state)]
+    (let [after-state (step-state before-state
+                                  crowded-seat?
+                                  no-adjacent-occupied?)]
+      (if (= before-state after-state)
+        (count-total-occupied after-state)
+        (recur after-state)))))
+
+(defn out-of-bounds?
+  [seats-state coords]
+  (let [[y x] coords
+        height (count seats-state)
+        width (count (first seats-state))]
+    (or (or (< x 0)
+            (> x width))
+        (or (< y 0)
+            (> y height)))))
+
+(defn update-coords-in-direction
+  [coords direction]
+  (let [[y x] coords]
+    (case direction
+      :nw [(- y 1) (- x 1)]
+      :n  [(- y 1) x]
+      :ne [(- y 1) (+ x 1)]
+      :w  [y (- x 1)]
+      :e  [y (+ x 1)]
+      :sw [(+ y 1) (- x 1)]
+      :s  [(+ y 1) x]
+      :se [(+ y 1) (+ x 1)])))
+
+(defn occupied-seat-in-direction?
+  [seats-state coords direction]
+  (loop [coords (update-coords-in-direction coords direction)]
+    (if (out-of-bounds? seats-state coords)
+      false
+      (let [seat-type (get-seat-at coords seats-state)]
+        (case seat-type
+          \# true
+          \L false
+          \. (recur (update-coords-in-direction coords direction))
+          false)))))
+
+(defn num-occuped-seats-in-each-direction
+  [seats-state seat-coords]
+  (let [directions [:nw :n :ne
+                    :w     :e
+                    :sw :s :se]]
+    (->> directions
+         (map #(occupied-seat-in-direction? seats-state seat-coords %))
+         (filter identity)
+         (count))))
+
+(defn crowded-seats-in-sight?
+  [seat-coords seats-state]
+  (< 4 (num-occuped-seats-in-each-direction seats-state seat-coords)))
+
+(defn no-occupied-in-sight?
+  [seat-coords seats-state]
+  (= 0 (num-occuped-seats-in-each-direction seats-state seat-coords)))
+
+(defn part2
+  [input]
+  (loop [before-state input]
+    (let [after-state (step-state before-state
+                                  crowded-seats-in-sight?
+                                  no-occupied-in-sight?)]
       (if (= before-state after-state)
         (count-total-occupied after-state)
         (recur after-state)))))
